31e86d8c8e106bff815221628a200b8691194655
Izumi <Izumi@DESKTOP-KND4VIK>
2022-04-03 11:45:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ChessCSharp.Piece
+{
+	public class pawn : Piece
+	{
+		public Tile currentTile = new Tile();
+		public Tile nextTile = new Tile();
+
+		bool IsValidMove(nextTile, currentTile)
+        {
+			if (Math.Abs(nextTile.position.x - currentTile.position.x) == 1 && Math.Abs(nextTile.position.y - currentTile.position.y) == 0)
+			{
+				return true;
+			}
+
+			else if (Math.Abs(nextTile.position.x - currentTile.position.x) == 1 && Math.Abs(nextTile.position.y - currentTile.position.y) == 1 && currentTile.IsEmpty == false)
+			{
+				return true;
+			}
+			else return false;
+
+		}
+	}
+}
